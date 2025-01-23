2380c47277e141794290f219a3d71dce0c20a733
Vương Văn Thành <123297460+ThanhhnanT@users.noreply.github.com>
2025-01-23 14:52:47 +0700

    done-khung-admin-product

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -1,7 +1,9 @@
 const dashboard = require("./dashboard.route")
+const product = require("./product.route")
 const systemConfig = require("../../config/system")
 module.exports = (app) => {
     const PATH_ADMIN = systemConfig.prefixAdmin;
 
     app.use(PATH_ADMIN + "/dashboard", dashboard)
+    app.use(PATH_ADMIN + "/product", product)
 }
