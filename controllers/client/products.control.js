80a10142a2d33985077d0f961c0f230d07afb020
Vương Văn Thành <123297460+ThanhhnanT@users.noreply.github.com>
2025-01-23 12:53:05 +0700

    draw-sp

--- controllers/client/products.control.js
+++ controllers/client/products.control.js
@@ -1,5 +1,19 @@
-module.exports.products = (req, res ) => {
+const Product = require('../../model/product.model')
+
+module.exports.products = async (req, res ) => {
+    const product = await Product.find({
+        status: "active",
+        deleted: false
+    })
+    const newProduct = product.map(item => {
+        item.priceNew = (item.price - item.price*item.discountPercentage/100).toFixed(2)
+        return item
+    })
+
     res.render('client/pages/product/product', {
         title: 'Sản phẩm',
+        products: newProduct
     })
+    
+    
 }
