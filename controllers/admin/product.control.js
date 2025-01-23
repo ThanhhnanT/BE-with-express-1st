73907662bd33e004a2735f11a229946cb96259d1
Vương Văn Thành <123297460+ThanhhnanT@users.noreply.github.com>
2025-01-23 15:16:05 +0700

    a

--- controllers/admin/product.control.js
+++ controllers/admin/product.control.js
@@ -1,5 +1,12 @@
-module.exports.index = (req, res) => {
+const Product = require("../../model/product.model")
+
+module.exports.index = async (req, res) => {
+    const products = await Product.find({
+        deleted: false
+    })
+    console.log(products)
     res.render("admin/pages/product/index.pug", {
-        pageTitle: "Admin: Trang sản phẩm"
+        pageTitle: "Admin: Trang sản phẩm",
+        product: products
     })
 }
