863c242228b49e7f54533b98220d42f716bf5449
Vương Văn Thành <123297460+ThanhhnanT@users.noreply.github.com>
2025-01-28 23:01:46 +0700

    a

--- controllers/client/products.control.js
+++ controllers/client/products.control.js
@@ -1,12 +1,12 @@
 const Product = require('../../model/product.model')
 
-module.exports.products = async (req, res ) => {
+module.exports.products = async (req, res) => {
     const product = await Product.find({
         status: "active",
         deleted: false
-    }).sort({position: "desc"})
+    }).sort({ position: "desc" })
     const newProduct = product.map(item => {
-        item.priceNew = (item.price - item.price*item.discountPercentage/100).toFixed(2)
+        item.priceNew = (item.price - item.price * item.discountPercentage / 100).toFixed(2)
         return item
     })
 
@@ -14,6 +14,26 @@ module.exports.products = async (req, res ) => {
         title: 'Sản phẩm',
         products: newProduct
     })
-    
-    
+
+
+}
+
+module.exports.detail = async (req, res) => {
+    try {
+        // console.log(req.params.slug)
+        const detailProduct = await Product.findOne(
+            {
+                deleted: false,
+                slug: req.params.slug,
+                status: "active"
+            }
+        )
+        // console.log(detailProduct)
+        res.render("client/pages/product/detail.pug", {
+            title: "Chi tiết sản phẩm",
+            item: detailProduct
+        })
+    } catch(e){
+        res.redirect("/product")
+    }
 }
--- routes/client/product.route.js
+++ routes/client/product.route.js
@@ -3,5 +3,7 @@ const router = express.Router()
 const controller = require('../../controllers/client/products.control.js')
 
 router.get('/', controller.products)
+router.get('/:slug', controller.detail)
+
 
 module.exports = router
